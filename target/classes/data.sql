9a3d3514c202b0b12d0d7ef7893e2b574b166247
manmeetdevgun <manmeet.devgun@nagarro.com>
2021-02-18 23:22:07 +0530

    update gitignore

---
+++ target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO SERVICE (name, is_available, pincode, description) VALUES ('Elecrical', 1, 110019, 'Description A');
+INSERT INTO SERVICE (name, is_available, pincode, description) VALUES ('Cleaning', 1, 110019, 'Description B');
+INSERT INTO SERVICE (name, is_available, pincode, description) VALUES ('Car Repair', 1, 110019,'Description C');
+INSERT INTO SERVICE (name, is_available, pincode, description) VALUES ('Drycleaning', 1, 110019,'Description D');
+INSERT INTO SERVICE (name, is_available, pincode, description) VALUES ('Spa', 1, 110019,'Description F');
